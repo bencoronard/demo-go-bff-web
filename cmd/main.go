9d50bdd8899c332d82085af2112a777b3c8fe405
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-04-09 22:07:28 +0700

    Reset project for new common lib convention

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,9 @@
 package main
 
 import (
-	"github.com/bencoronard/demo-go-bff-web/internal/config"
-	"github.com/bencoronard/demo-go-bff-web/internal/permission"
-	"github.com/bencoronard/demo-go-bff-web/internal/token"
-	"github.com/bencoronard/demo-go-common-libs/http"
-	"github.com/bencoronard/demo-go-common-libs/otel"
 	"go.uber.org/fx"
 )
 
 func main() {
-	fx.New(
-		fx.Provide(
-			config.NewProperties,
-			config.NewDB,
-			config.NewJwtIssuer,
-			config.NewRouter,
-		),
-		fx.Provide(
-			token.NewTokenService,
-			token.NewTokenHandler,
-			permission.NewPermissionRepo,
-		),
-		fx.Provide(
-			config.NewResource,
-			otel.NewTracerProvider,
-			otel.NewMeterProvider,
-			otel.NewLoggerProvider,
-		),
-		fx.Provide(
-			http.NewGlobalErrorHandler,
-		),
-		fx.Invoke(
-			config.ConfigureLogger,
-			otel.InitOtel,
-			http.Router.RegisterMiddlewares,
-			http.Router.RegisterRoutes,
-		),
-		fx.Invoke(http.Start),
-	).Run()
+	fx.New().Run()
 }
