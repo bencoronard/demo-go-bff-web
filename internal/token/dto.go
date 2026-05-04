bdbd9ad41d22f8262361c238bfd6b4ebd4142bec
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-05-04 14:32:49 +0700

    Add validation mesage to DTOs

--- internal/token/dto.go
+++ internal/token/dto.go
@@ -1,6 +1,6 @@
 package token
 
 type issueTokenDTO struct {
-	Id  string `validate:"required,numeric,gt=0"`
-	Ttl string `validate:"required,numeric,gt=0"`
+	Id  string `validate:"required,numeric,gt=0" msg:"ID must be a positive number"`
+	Ttl string `validate:"required,numeric,gt=0" msg:"TTL must be a positive number"`
 }
